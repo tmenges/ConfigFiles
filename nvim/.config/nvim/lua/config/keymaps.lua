9163cad71dccb4aea73da5314865aff9a0c692ba
Tony Menges <tony.menges@gmail.com>
2024-02-28 17:48:32 -0800

    fix harpoon key mappings

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -13,35 +13,20 @@ vim.keymap.set("n", "<C-u>", "<C-u>zzzv", { noremap = true, silent = true })
 
 -- Keymaps for harpoon plugin
 local harpoon = require("harpoon")
-
--- REQUIRED
-harpoon:setup()
--- REQUIRED
+--harpoon:setup()
 
 vim.keymap.set("n", "<leader>a", function()
   harpoon:list():append()
-end)
+end, { desc = "Add to Harpoon" })
+
 vim.keymap.set("n", "<C-e>", function()
   harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
-
-vim.keymap.set("n", "<C-h>", function()
-  harpoon:list():select(1)
-end)
-vim.keymap.set("n", "<C-t>", function()
-  harpoon:list():select(2)
-end)
-vim.keymap.set("n", "<C-n>", function()
-  harpoon:list():select(3)
-end)
-vim.keymap.set("n", "<C-s>", function()
-  harpoon:list():select(4)
-end)
-
+end, { desc = "Show Harpoon" })
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-S-P>", function()
   harpoon:list():prev()
-end)
+end, { desc = "Next Harpoon buffer" })
+
 vim.keymap.set("n", "<C-S-N>", function()
   harpoon:list():next()
-end)
+end, { desc = "Previous Harpoon buffer" })
